fc7d117ebd3dc1bc5c0967161524abc5dae5c4aa
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-02-03 22:35:53 -0600

    Updates

--- js/app.js
+++ js/app.js
@@ -36,18 +36,19 @@ function displayResults (weather) {
 
     periodoTiempo(weather.weather[0].icon);
 
-    let city = document.querySelector('.location .city');
-    city.innerHTML = `${weather.name}<sub>${weather.sys.country}</sub>`;
-    
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
+
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+    
 
     let icon = document.querySelector('.current .icon');
     icon.setAttribute('src', `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather.weather[0].icon}.svg`);
     
     let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<sub>°c</sub>`;
+    temp.innerHTML = `${Math.round(weather.main.temp)} °c`;
     
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
@@ -91,8 +92,10 @@ function toastAlert () {
 function periodoTiempo (tiempo) {
     const card = document.querySelector('.card');
     if (tiempo.includes('n')) {
-        card.style.backgroundImage = 'url(./img/night.jpg)';
+        card.classList.remove('day');
+        card.classList.add('night');
     } else if (tiempo.includes('d')) {
-        card.style.backgroundImage = 'url(./img/day.jpg)';
+        card.classList.remove('night');
+        card.classList.add('day');
     }
 }
